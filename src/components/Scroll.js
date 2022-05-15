30ee1c8f668c6e50a2b31097a95a7a31d01bd93d
unknown <jlbmontejano@gmail.com>
2022-05-15 12:31:37 -0700

    add redux dependency

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = props => {
     return (
-        <div style={{ overflow: 'scroll', border: '3px solid black', height: '700px' }}>
+        <div style={{ overflow: 'scroll', height: '700px' }}>
             {props.children}
         </div>
     );
