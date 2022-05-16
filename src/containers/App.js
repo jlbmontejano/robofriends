cfbe4d5cf7fa77ea931002d081b97fa4f69707be
unknown <jlbmontejano@gmail.com>
2022-05-15 17:06:22 -0700

    deleted scroll components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
-import Scroll from "../components/Scroll";
-import ErrorBoundry from "../components/ErrorBoundry";
 import "./App.css";
 
 const App = () => {
   const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState('');
+  const [searchfield, setSearchfield] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -29,11 +27,7 @@ const App = () => {
     <div className="tc">
       <h1 className="f1">RoboFriends</h1>
       <SearchBox searchChange={onSearchChange} />
-      <Scroll>
-        <ErrorBoundry>
-          <CardList robots={filteredRobots} />
-        </ErrorBoundry>
-      </Scroll>
+      <CardList robots={filteredRobots} />
     </div>
   );
 };
